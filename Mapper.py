bf86ca167976ed59a6e654cd98126d0c54187270
Haonan Yang <12487476+rapsoulhaonan@users.noreply.github.com>
2017-09-14 08:59:24 -0400

    Create Mapper.py

---
+++ Mapper.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+num_nodes = len(sys.argv)
+
+for line in sys.stdin:            # go through graph
+   line = line.strip()
+   words = line.split()
+   origin = long(words[0])
+   target = long(words[1])
+   
+   for i in range(1,num_nodes):  # find neighbors of given node
+      if long(sys.argv[i]) == origin:
+         print '%d' % (target)       # map neighbor nodes
